026e4e7ac1f7f2896e6228c6634651bf0afcf689
Chris Zimmerman <chrisxzimmerman@gmail.com>
2015-07-05 18:43:01 -0400

    Fixed a typo.

---
+++ FriendGatherer/Globals/Constants.cs
@@ -0,0 +1,10 @@
+﻿namespace FriendWrangler.Globals
+{
+    public static class Constants
+    {
+        public static class Friends
+        {
+            public static string DefaultEmail = "no email";    
+        }
+    }
+}

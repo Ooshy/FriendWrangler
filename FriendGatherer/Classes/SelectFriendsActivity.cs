0b7bbd85654e90d6d2f7a04e491532eefba643c0
Chris Zimmerman <chrisxzimmerman@gmail.com>
2015-07-28 22:50:01 -0400

    Changed color scheme. Integrated contacts list into app.

--- FriendGatherer/Classes/SelectFriendsActivity.cs
+++ FriendGatherer/Classes/SelectFriendsActivity.cs
@@ -2,10 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Android.App;
+using Android.OS;
+using Android.Widget;
 
 namespace FriendWrangler.Classes
 {
-    class SelectFriendsActivity
+    [Activity(Label = "Friend Selection")]
+    public class SelectFriendsActivity : Activity
     {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.SelectFriends);
+            var contactsAdapter = new ContactsAdapter(this);
+            var contactsListView = FindViewById<ListView>(Resource.Id.ContactsListView);
+            contactsListView.Adapter = contactsAdapter;
+        }
     }
 }
